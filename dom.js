99cd4908e67c610667d4c28eebd3bcc661225c0b
extremistLab <adarsh.chb@gmail.com>
2023-08-15 00:39:49 +0530

    done !!!!

---
+++ dom.js
@@ -0,0 +1,25 @@
+//console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+
+// console.log(document.title);
+// //document.title=568;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+var addItem = document.getElementById('add-items');
+// console.log(headerTitle);
+// headerTitle.textContent ='Hello';
+
+headerTitle.style.borderBottom = 'solid 3px #000';
+addItem.style.fontWeight ='bold';
+addItem.style.color ='green';
+
