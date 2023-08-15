31080a014a5af8dcce8a4dc2bf7064e66f77e0db
extremistLab <adarsh.chb@gmail.com>
2023-08-15 14:46:48 +0530

    filterFeature

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
 
 function addItem(e){
     e.preventDefault();
@@ -44,3 +48,20 @@ function removeItem(e){
         }
     console.log(1);
 }
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
